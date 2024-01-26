4bd592b004144d3c96cc2b204b886f4edfe0793e
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-01-26 16:09:39 +0300

    v2

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,7 +1,11 @@
 package com.tpe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/students")//http:localhost:8080/SpringMvc/students/......
+//class:tüm metodlar için geçerlidir.
+//method:sadece ilgili metod için geçerli olur.
 public class StudentController {
 }
---
+++ src/main/java/com/tpe/repository/StudentRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.tpe.repository;
+
+public class StudentRepositoryImpl {
+
+
+
+}
